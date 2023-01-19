60c773b05823811405f6fdc7e282cc91f883ee24
Ryuzo-hirota <49504012+Ryuzo-hirota@users.noreply.github.com>
2023-01-19 23:22:34 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-window.onload = ()=> {
-  let url = location.href;
-  if (url == "https://mobile.twitter.com/home") {
-    window.setTimeout(hoge, 1000);
 
-    function hoge(){
-        document.querySelectorAll('div[data-testid=ScrollSnap-List] a[href="/home"]')[1].click();
-    }
-  } 
-}
+let url = location.href;
+if (url == "https://mobile.twitter.com/home") {
+  window.setTimeout(hoge, 1000);
+  function hoge(){
+      document.querySelectorAll('div[data-testid=ScrollSnap-List] a[href="/home"]')[1].click();
+  }
+} 
