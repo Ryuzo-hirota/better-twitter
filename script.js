1bc2d22c33d53eefc45d94d236d7dbb09cff1170
Ryuzo-hirota <49504012+Ryuzo-hirota@users.noreply.github.com>
2023-01-19 22:53:50 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+window.onload = ()=> {
+  // URLの取得
+  let url = location.href;
+  if (url == "http://mobile.twitter.com/home") {
+    window.setTimeout(hoge, 1000);
+
+    function hoge(){
+        document.querySelectorAll('div[data-testid=ScrollSnap-List] a[href="/home"]')[1].click();
+    }
+  } 
+}
